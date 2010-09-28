831f9bed631e6dac3e96bb4c6eccf75eb3e3f733
teejayvanslyke <teejay.vanslyke@gmail.com>
2010-09-28 08:55:14 -0700

    Added Enigma::Client class to allow easily creating client connections

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -32,4 +32,59 @@ module Enigma
 
     matched
   end
+
+	class Client 
+		def initialize(options)
+			@host        = options[:host] || 'localhost'
+			@port        = options[:port] || 80
+			@ssl         = options[:ssl]  || false
+			@certificate = options[:certificate]
+		end
+
+		attr_reader :host, :port, :ssl, :certificate, :version
+
+		HTTP_METHODS = [:get, :post, :put, :delete]
+		HTTP_METHODS.each do |method|
+			define_method(method) do |path, *args|
+				payload = args.first
+				execute method, path, payload
+			end
+		end
+
+		def http
+			base = "#{ssl ? "https" : "http"}://#{host}:#{port}"
+			Class.new do
+				include HTTParty
+				base_uri base
+			end
+		end
+
+		def execute(method, path, payload)
+			response = http.send(method, path,
+													 :headers => headers(method, path, payload),
+													 :body => payload,
+													 :timeout => TIMEOUT
+													)
+
+													response.body
+		end
+
+		TIMEOUT = 60 * 5
+
+		private
+
+		def headers(method, url, payload)
+			{Enigma::SIGNATURE_HEADER =>
+				Enigma.signature(method, url, payload)
+			}
+		end
+
+		def scheme
+			@ssl ? 'https' : 'http'
+		end
+
+		def base_url
+			"#{scheme}://#{host}:#{port}"
+		end
+	end
 end
